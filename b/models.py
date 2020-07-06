45413bdf845772f46906343de99c0acee97d8c47
m1ramira <mironova3456@gmail.com>
2020-07-06 15:42:25 +0300

    added draft and login

--- b/models.py
+++ b/models.py
@@ -16,3 +16,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('b.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approve_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approve_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
